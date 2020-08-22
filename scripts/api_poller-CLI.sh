59689cb35eab1ece0c2d53b247251b4a69d59b9c
David Brunnthaler <david@screencode.at>
2020-08-22 19:25:20 +0200

    use the direnv variables even when called from systemctl

--- scripts/api_poller-CLI.sh
+++ scripts/api_poller-CLI.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+. /home/plex/Documents/Scripts/fitbit/scripts/.envrc
 
 source /home/plex/Documents/Scripts/fitbit/python-venv/bin/activate && python /home/plex/Documents/Scripts/fitbit/scripts/api_poller.py
